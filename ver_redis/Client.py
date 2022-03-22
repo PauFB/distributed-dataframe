5973447c1684b017acd238083b30779bebc6506e
Pau Fernandez Bardulet <paufernandezbardulet@outlook.com>
2022-03-22 19:50:25 +0100

    22/3/2022

--- ver_redis/Client.py
+++ ver_redis/Client.py
@@ -4,84 +4,129 @@ import pandas as pd
 import redis
 
 master = redis.from_url('redis://localhost:6379', db=0)
-
-client_worker_list = []
-i = 0
-while i < master.llen("workers"):
-    client_worker_list.append(redis.from_url(master.lindex("workers", i).decode("utf-8")))
-    i += 1
-print(client_worker_list)
+n_workers = master.pubsub_numsub("methods")[0][1]
 
 # Read CSV
-csv_list = ["../resources/df1.csv", "../resources/df2.csv"]
-
-i = 0
-for worker in client_worker_list:
-    worker.set("df", pickle.dumps(pd.read_csv(csv_list[i])))
-    i += 1
+master.publish("methods", "read_csv;../resources/df1.csv,../resources/df2.csv")
 
 # apply
 print("\nTest apply(lambda x: x + 2)")
+
+master.publish("methods", "apply;lambda x: x + 2")
+
+n_results = master.llen("results")
+while n_results != n_workers:
+    n_results = master.llen("results")
+
 result = pd.DataFrame(columns=['x', 'y', 'z'])
-for worker in client_worker_list:
-    result = pd.concat([result, pickle.loads(worker.get("df")).apply(eval("lambda x: x + 2"))])
+for i in range(n_results):
+    result = pd.concat([result, pickle.loads(master.lpop("results"))])
 print(result)
 
 # columns
 print("\nTest columns()")
+
+master.publish("methods", "columns")
+
+n_results = master.llen("results")
+while n_results != n_workers:
+    n_results = master.llen("results")
+
 result = pd.Index([])
-for worker in client_worker_list:
-    result = result.union(pickle.loads(worker.get("df")).columns)
+for i in range(n_results):
+    result = result.union(pickle.loads(master.lpop("results")))
 print(result)
 
 # groupby
 print("\nTest groupby(z).sum()")
+
+master.publish("methods", "groupby;z")
+
+n_results = master.llen("results")
+while n_results != n_workers:
+    n_results = master.llen("results")
+
 result = pd.DataFrame(columns=['x', 'y'])
-for worker in client_worker_list:
-    result = pd.concat([result, pickle.loads(worker.get("df")).groupby('z').sum()])
+for i in range(n_results):
+    result = pd.concat([result, pickle.loads(master.lpop("results")).sum()])
 print(result)
 
 # head
 print("\nTest head()")
+
+master.publish("methods", "head;4")
+
+n_results = master.llen("results")
+while n_results != n_workers:
+    n_results = master.llen("results")
+
 result = pd.DataFrame(columns=['x', 'y', 'z'])
-for worker in client_worker_list:
-    result = pd.concat([result, pickle.loads(worker.get("df")).head()])
+for i in range(n_results):
+    result = pd.concat([result, pickle.loads(master.lpop("results"))])
 print(result)
 
 # isin
 print("\nTest isin([2, 4])")
+
+master.publish("methods", "isin;2,4")
+
+n_results = master.llen("results")
+while n_results != n_workers:
+    n_results = master.llen("results")
+
 result = pd.DataFrame(columns=['x', 'y', 'z'])
-for worker in client_worker_list:
-    result = pd.concat([result, pickle.loads(worker.get("df")).isin([2, 4])])
+for i in range(n_results):
+    result = pd.concat([result, pickle.loads(master.lpop("results"))])
 print(result)
 
 # items
-print("\nTest items")
-for worker in client_worker_list:
-    for label, content in pickle.loads(worker.get("df")).items():
-        print(f'label: {label}')
-        print(f'content: {content}', sep='\n')
+print("\nTest items()")
+
+master.publish("methods", "items")
+
+n_results = master.llen("results")
+while n_results != n_workers:
+    n_results = master.llen("results")
+
+result = ''
+for i in range(n_results):
+    result = result + pickle.loads(master.lpop("results")) + "\n"
+print(result)
 
 # max
 print("\nTest max(0)")
+
+master.publish("methods", "max;0")
+
+n_results = master.llen("results")
+while n_results != n_workers:
+    n_results = master.llen("results")
+
 result = pd.Series([0, 0, 0], index=['x', 'y', 'z'])
-for worker in client_worker_list:
-    max_wk = pickle.loads(worker.get("df")).max(0)
-    i = 0
+for i in range(n_results):
+    max_wk = pickle.loads(master.lpop("results"))
+    j = 0
     for index, value in max_wk.items():
-        if value > result[i]:
-            result[i] = value
-        i += 1
+        if value > result[j]:
+            result[j] = value
+        j += 1
 print(result)
 
 # min
 print("\nTest min(0)")
+
+master.publish("methods", "min;0")
+
+n_results = master.llen("results")
+while n_results != n_workers:
+    n_results = master.llen("results")
+
 result = pd.Series([999, 999, 999], index=['x', 'y', 'z'])
-for worker in client_worker_list:
-    max_wk = pickle.loads(worker.get("df")).min(0)
-    i = 0
+for i in range(n_results):
+    max_wk = pickle.loads(master.lpop("results"))
+    j = 0
     for index, value in max_wk.items():
-        if value < result[i]:
-            result[i] = value
-        i += 1
+        if value < result[j]:
+            result[j] = value
+        j += 1
 print(result)
