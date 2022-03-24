c4088585a5dcb63fb5c9287dfd47294ef4a0c95c
Pau Fernandez Bardulet <paufernandezbardulet@outlook.com>
2022-03-24 12:55:33 +0100

    24/3/2022

--- ver_rabbitmq/Client.py
+++ ver_rabbitmq/Client.py
@@ -1,17 +1,30 @@
+import pickle
+
+import pandas
 import pika
 
-connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
 channel = connection.channel()
+worker_exchange = channel.exchange_declare(exchange='worker_exchange', exchange_type='fanout')
+response_queue = channel.queue_declare(queue='response_queue')
 
-channel.queue_declare(queue='master_queue')
+channel.queue_purge(queue='response_queue')
 
-channel.basic_publish(exchange='', routing_key='master_queue', body="Hola")
-print("Sent message: Hola")
+# Read CSV
+channel.basic_publish(exchange='worker_exchange', routing_key='', body="read_csv('../df.csv')")
 
-channel.basic_publish(exchange='', routing_key='master_queue', body="Hola1")
-print("Sent message: Hola1")
+# columns()
+channel.basic_publish(exchange='worker_exchange', routing_key='', body="columns()")
+while channel.queue_declare(queue='response_queue').method.message_count > 0:
+    worker_result = pickle.loads(channel.basic_get(queue='response_queue')[2])
+    print(worker_result)
 
-channel.basic_publish(exchange='', routing_key='master_queue', body="Hola2")
-print("Sent message: Hola2")
+# head()
+print("\n--- head(5) test ---")
+dataframe = pandas.DataFrame()
+channel.basic_publish(exchange='worker_exchange', routing_key='', body="head(5)")
+while channel.queue_declare(queue='response_queue').method.message_count > 0:
+    dataframe = pandas.concat([dataframe, pickle.loads(channel.basic_get(queue='response_queue')[2])])
+print(dataframe)
 
 connection.close()
---
+++ ver_rabbitmq/Worker2.py
@@ -0,0 +1,68 @@
+import pickle
+
+import pandas
+import pika
+
+df = pandas.DataFrame()
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+channel = connection.channel()
+channel.exchange_declare(exchange='worker_exchange', exchange_type='fanout')
+channel.queue_declare(queue='response_queue')
+result = channel.queue_declare(queue='', exclusive=True)
+queue_name = result.method.queue
+channel.queue_bind(exchange='worker_exchange', queue=queue_name)
+
+
+def read_csv(url):
+    global df
+    df = pandas.read_csv(url, skiprows=[i for i in range(1, 5)], skipfooter=4, engine='python')
+
+
+def apply(func):
+    return df.apply(eval(func))
+
+
+def columns():
+    return df.columns
+
+
+def groupby(by):
+    return df.groupby(by)
+
+
+def head(n=5):
+    return df.head(n)
+
+
+def isin(values):
+    return df.isin(values)
+
+
+def items():
+    aux = ''
+    for label, content in df.items():
+        aux += f'label: {label}\n'
+        aux += f'content:\n'
+        for c in content:
+            aux += f'{c}\n'
+    return aux
+
+
+def max(axis):
+    return df.max(axis)
+
+
+def min(axis):
+    return df.min(axis)
+
+
+def callback(ch, method, properties, body):
+    response = pickle.dumps(eval(body))
+    channel.basic_publish(exchange='', routing_key='response_queue', body=response)
+
+
+channel.basic_consume(
+    queue=queue_name, on_message_callback=callback, auto_ack=True)
+
+channel.start_consuming()
