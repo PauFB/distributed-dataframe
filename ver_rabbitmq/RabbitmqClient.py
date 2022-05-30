b8d06410c79a6c59fa8855bc5fd7b51b3a42f875
Marc Beltran <marcbeltran4@gmail.com>
2022-05-30 17:32:52 +0200

    RabbitMQ consumers count

--- ver_rabbitmq/RabbitmqClient.py
+++ ver_rabbitmq/RabbitmqClient.py
@@ -3,6 +3,7 @@ import sys
 import time
 import pandas as pd
 import pika
+import os
 
 start = time.time()
 
@@ -13,6 +14,11 @@ response_queue = channel.queue_declare(queue='response_queue')
 
 channel.queue_purge(queue='response_queue')
 
+
+stream = os.popen('curl -i -u guest:guest http://localhost:15672/api/exchanges/%2F/worker_exchange/bindings/source')
+output = stream.read()
+n_workers = output.count('source')
+
 # Read CSV
 channel.basic_publish(exchange='worker_exchange', routing_key='', body="read_csv('../df.csv')")
 
@@ -20,7 +26,7 @@ channel.basic_publish(exchange='worker_exchange', routing_key='', body="read_csv
 print("\nTest apply(lambda x: x + 2)")
 result = pd.DataFrame(columns=['x', 'y', 'z'])
 channel.basic_publish(exchange='worker_exchange', routing_key='', body="apply(\"lambda x: x + 2\")")
-while channel.queue_declare(queue='response_queue').method.message_count < 3:
+while channel.queue_declare(queue='response_queue').method.message_count != n_workers:
     pass
 while channel.queue_declare(queue='response_queue').method.message_count > 0:
     result = pd.concat([result, pickle.loads(channel.basic_get(queue='response_queue')[2])])
@@ -29,7 +35,7 @@ print(result)
 # columns()
 print("\nTest columns()")
 channel.basic_publish(exchange='worker_exchange', routing_key='', body="columns()")
-while channel.queue_declare(queue='response_queue').method.message_count < 3:
+while channel.queue_declare(queue='response_queue').method.message_count != n_workers:
     pass
 while channel.queue_declare(queue='response_queue').method.message_count > 0:
     worker_result = pickle.loads(channel.basic_get(queue='response_queue')[2])
@@ -39,7 +45,7 @@ while channel.queue_declare(queue='response_queue').method.message_count > 0:
 print("\nTest groupby(z).sum()")
 result = pd.DataFrame(columns=['x', 'y'])
 channel.basic_publish(exchange='worker_exchange', routing_key='', body="groupby(\"z\").sum()")
-while channel.queue_declare(queue='response_queue').method.message_count < 3:
+while channel.queue_declare(queue='response_queue').method.message_count != n_workers:
     pass
 while channel.queue_declare(queue='response_queue').method.message_count > 0:
     result = pd.concat([result, pickle.loads(channel.basic_get(queue='response_queue')[2])])
@@ -49,7 +55,7 @@ print(result)
 print("\nTest head(5)")
 dataframe = pd.DataFrame()
 channel.basic_publish(exchange='worker_exchange', routing_key='', body="head(5)")
-while channel.queue_declare(queue='response_queue').method.message_count < 3:
+while channel.queue_declare(queue='response_queue').method.message_count != n_workers:
     pass
 while channel.queue_declare(queue='response_queue').method.message_count > 0:
     dataframe = pd.concat([dataframe, pickle.loads(channel.basic_get(queue='response_queue')[2])])
@@ -59,7 +65,7 @@ print(dataframe)
 print("\nTest isin([2, 4])")
 result = pd.DataFrame(columns=['x', 'y', 'z'])
 channel.basic_publish(exchange='worker_exchange', routing_key='', body="isin([2, 4])")
-while channel.queue_declare(queue='response_queue').method.message_count < 3:
+while channel.queue_declare(queue='response_queue').method.message_count != n_workers:
     pass
 while channel.queue_declare(queue='response_queue').method.message_count > 0:
     result = pd.concat([result, pickle.loads(channel.basic_get(queue='response_queue')[2])])
@@ -69,7 +75,7 @@ print(result)
 print("\nTest items")
 result = ''
 channel.basic_publish(exchange='worker_exchange', routing_key='', body="items()")
-while channel.queue_declare(queue='response_queue').method.message_count < 3:
+while channel.queue_declare(queue='response_queue').method.message_count != n_workers:
     pass
 while channel.queue_declare(queue='response_queue').method.message_count > 0:
     result = result + "\n" + pickle.loads(channel.basic_get(queue='response_queue')[2])
@@ -79,7 +85,7 @@ print(result)
 print("\nTest max(0)")
 result = pd.Series([-sys.maxsize - 1, -sys.maxsize - 1, -sys.maxsize - 1], index=['x', 'y', 'z'])
 channel.basic_publish(exchange='worker_exchange', routing_key='', body="max(0)")
-while channel.queue_declare(queue='response_queue').method.message_count < 3:
+while channel.queue_declare(queue='response_queue').method.message_count != n_workers:
     pass
 while channel.queue_declare(queue='response_queue').method.message_count > 0:
     max_wk = pickle.loads(channel.basic_get(queue='response_queue')[2])
@@ -94,7 +100,7 @@ print(result)
 print("\nTest min(0)")
 result = pd.Series([sys.maxsize, sys.maxsize, sys.maxsize], index=['x', 'y', 'z'])
 channel.basic_publish(exchange='worker_exchange', routing_key='', body="min(0)")
-while channel.queue_declare(queue='response_queue').method.message_count < 3:
+while channel.queue_declare(queue='response_queue').method.message_count != n_workers:
     pass
 while channel.queue_declare(queue='response_queue').method.message_count > 0:
     max_wk = pickle.loads(channel.basic_get(queue='response_queue')[2])
@@ -106,6 +112,6 @@ while channel.queue_declare(queue='response_queue').method.message_count > 0:
 print(result)
 
 finish = time.time()
-print("Execution time: " + str(finish-start))
+print("Execution time: " + str(finish - start))
 
 connection.close()
